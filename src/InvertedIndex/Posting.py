3d6d009da649cd4773d47743e88a0924a12f8296
Pietrangelo Manco <p.manco@studenti.unipi.it>
2024-12-28 14:30:49 +0100

    Changed folder structure

--- src/InvertedIndex/Posting.py
+++
@@ -1,7 +0,0 @@
-class Posting:
-    def __init__(self, doc_id: int, payload: int):
-        """
-        Posting object class.
-        """
-        self.doc_id = doc_id
-        self.payload = payload
